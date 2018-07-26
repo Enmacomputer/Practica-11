6c831a141f03354aa729caf7b423761ba2dbbfa5
Enmacomputer <40839110+Enmacomputer@users.noreply.github.com>
2018-07-26 18:28:07 -0400

    Datos

---
+++ practica 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numero;
+			Console.WriteLine("Digite numero");
+			numero = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine();
+
+			for (int i = 1; i < 12; i++)
+			{
+				int Multiplicar = numero * i;
+				Console.WriteLine("{0}x{1}={2}", numero, i, Multiplicar);
+			}
+			Console.ReadKey();
+
+
+		}
+	}
+}
